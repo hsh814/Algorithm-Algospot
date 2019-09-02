0be41afc3790977252672b0b688911cb354f4a92
hsh814 <hsh814@naver.com>
2019-09-02 21:10:52 +0900

    input

---
+++ boggle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int total;
+    cin >> totalCase;
+    char[5][5] boggle;
+    int i = 0;
+    while(i < totalCase){
+        cin >> char[i];
+        i++;
+    }
+    int wordsNum;
+    cin >> wordsNum;
+    string[] words = new string[wordsNum];
+    for(int j = 0; j < wordsNum; j++){
+        cin >> words[j];
+    }
+    bool[] results = new bool[wordsNum];
+    for(int k = 0; k < wordsNum; k++)
+    {
+        if (hasWord(string& str, int x, int y);
+    }
+    return 0;
+}
