aead168fcd10f7cc367c282bc701f75ea2597125
hsh814 <hsh814@naver.com>
2019-09-03 21:09:06 +0900

    First try

--- boggle.cpp
+++ boggle.cpp
@@ -3,26 +3,59 @@
 
 using namespace std;
 
+bool hasWord(const string& str, int x, int y);
+bool outOfRange(int x, int y);
+
+string *boggle = new string[5];
+
 int main()
 {
-    int total;
+    int totalCase;
     cin >> totalCase;
-    char[5][5] boggle;
-    int i = 0;
-    while(i < totalCase){
-        cin >> char[i];
-        i++;
+
+    string temp = "";
+    for(int i = 0; i < 5; i++){
+        cin >> boggle[i];
     }
+
     int wordsNum;
     cin >> wordsNum;
-    string[] words = new string[wordsNum];
+    string* words = new string[wordsNum];
     for(int j = 0; j < wordsNum; j++){
         cin >> words[j];
     }
-    bool[] results = new bool[wordsNum];
+    bool* results = new bool[wordsNum];
+
     for(int k = 0; k < wordsNum; k++)
     {
-        if (hasWord(string& str, int x, int y);
+        for(int x = 0; x < 5; x++){
+            for(int y = 0; y < 5; y++){
+                if (hasWord(words[k], x, y));
+            }
+        }
     }
     return 0;
+}
+
+int dx[8] = {1, 1, 1, 0, -1, -1, -1, 0};
+int dy[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+bool hasWord(const string& str, int x, int y)
+{
+    if(x > 4 || x < 0 || y > 4 || y < 0){
+        return false;
+    } else if (boggle[y][x] == str[0]) {
+        if(str.length <= 1){
+            return true;
+        }
+        string newStr = str.substr(1, str.length);
+        for(int i = 0; i < 8; i++){
+            if(hasWord(newStr, x + dx[i], y + dy[i])){
+                return true;
+            }
+            else { 
+                false;
+            }
+        }
+    }
+    return false;
 }
