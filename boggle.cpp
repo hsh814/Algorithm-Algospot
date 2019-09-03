33e1078d9a462eb4e32d94c77c5dd8803b96fe41
hsh814 <hsh814@naver.com>
2019-09-03 21:23:04 +0900

    Passed example case

--- boggle.cpp
+++ boggle.cpp
@@ -28,10 +28,20 @@ int main()
 
     for(int k = 0; k < wordsNum; k++)
     {
+        results[k] = false;
         for(int x = 0; x < 5; x++){
             for(int y = 0; y < 5; y++){
-                if (hasWord(words[k], x, y));
+                if (hasWord(words[k], x, y)){
+                    results[k] = true;
+                }
             }
+        }
+    }
+    for(int i = 0; i < wordsNum; i++){
+        if(results[i] == true){
+            cout << words[i] << " YES\n"; 
+        } else {
+            cout << words[i] << " NO\n";
         }
     }
     return 0;
@@ -39,15 +49,17 @@ int main()
 
 int dx[8] = {1, 1, 1, 0, -1, -1, -1, 0};
 int dy[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
 bool hasWord(const string& str, int x, int y)
 {
     if(x > 4 || x < 0 || y > 4 || y < 0){
         return false;
     } else if (boggle[y][x] == str[0]) {
-        if(str.length <= 1){
+        if(str.length() <= 1){
             return true;
         }
-        string newStr = str.substr(1, str.length);
+        string newStr = str.substr(1, str.length());
+        //cout << newStr << endl;
         for(int i = 0; i < 8; i++){
             if(hasWord(newStr, x + dx[i], y + dy[i])){
                 return true;
