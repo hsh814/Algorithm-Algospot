60bcb2f0ffeeeb19b79997cab86d98bbc1309a06
hsh814 <hsh814@naver.com>
2019-09-03 23:56:50 +0900

    In process..

--- picnic.cpp
+++ picnic.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentNum);
+int maxPair(int studentNum);
 
 int main()
 {
@@ -30,6 +31,15 @@ int main()
     return 0;
 }
 
+int maxPair(int studentNum)
+{
+    int result = 1;
+    for(int i = 0; i < studentNum; i += 2)
+    {
+        result *= (i + 1);
+    }
+    return result;
+}
 int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentNum)
 {
     bool isEveryonePaired = false;
@@ -47,19 +57,30 @@ int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentN
             return 0;
         }
     }
+    bool changed = true;
     bool* isPaired = new bool[studentNum]; 
-    while(true)
+    int result = 0;
+    int *firstY = new int[pairNum];
+    int start = 0;
+    for(int n = 0; n < pairNum; n++){
+        firstY[n] = 0;
+    }
+    for(int m = 0; m < maxPair(studentNum); m++)
     {
         for(int i = 0; i < studentNum; i++){
             isPaired[i] = false;
         }
         for(int x = 0; x < studentNum; x++)
         {
-            for(int y = 0; y < pairNum; y++)
+            if(isPaired[x] == true){
+                continue;
+            } else if(changed == true){
+                start = firstY[x];
+            } else {
+                start = firstY[x] + 1;
+            }
+            for(int y = start; y < pairNum; y++)
             {
-                if(isPaired[x] == true){
-                    continue;
-                }
                 if(x == pairs1[y] && isPaired[pairs2[y]] == false)
                 {
                     isPaired[x] = true; isPaired[pairs2[y]] = true;
@@ -68,6 +89,12 @@ int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentN
                 {
                     isPaired[x] == true; isPaired[pairs1[y]] == true;
                 }
+                if(y != firstY[x]){
+                    changed = true;
+                } else {
+                    changed = false;
+                }
+                firstY[x] = y;
             }
         }
         bool success = true;
@@ -76,8 +103,9 @@ int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentN
                 success = false; break;
             }
         }
+        if(success == true){
+            result++;
+        }
     }
-
-
-    return 0;
+    return result;
 }
