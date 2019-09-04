1c66f4f3d535f5efebd1b38ac5990a34e4f78f49
hsh814 <hsh814@naver.com>
2019-09-04 21:07:26 +0900

    fixed howMany()

--- picnic.cpp
+++ picnic.cpp
@@ -3,26 +3,61 @@
 
 using namespace std;
 
-int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentNum);
-int maxPair(int studentNum);
+int compare(int x1, int x2, int y1, int y2);
+int howMany(const int* pairs1, const int* pairs2, bool isPaired[], bool isFriend[][10], int pairNum, int studentNum);
 
 int main()
 {
+    //input
     int totalCase;
     cin >> totalCase;
     int results[50];
+    int a, b;
     for(int repeat = 0; repeat < totalCase; repeat++){
         int studentNum, pairNum;
         cin >> studentNum >> pairNum;
         int* pairs1 = new int[pairNum];
         int* pairs2 = new int[pairNum];
         for(int i = 0; i < pairNum; i++){
-            cin >> pairs1[i] >> pairs2[i];
+            cin >> a >> b;
+            if(a > b){
+                pairs1[i] = b; pairs2[i] = a;
+            } else {
+                pairs1[i] = a; pairs2[i] = b;
+            }
+        }
+        //Selection sort
+        int temp, end;
+        for(int x = 0; x < pairNum; x++)
+        {
+            for(int y = x + 1; y < pairNum; y++)
+            {
+                if(compare(pairs1[x], pairs2[x], pairs1[y], pairs2[y]) > 0)
+                {
+                    temp = pairs1[x]; pairs1[x] = pairs1[y]; pairs1[y] = temp;
+                    temp = pairs2[x]; pairs2[x] = pairs2[y]; pairs2[y] = temp; 
+                }
+            }
+        }
+        //output
+        bool isPaired[10] = {false};
+        bool isFriend[10][10] = {false};
+        for(int i = 0; i < studentNum; i++)
+        {
+            for(int j = i + 1; j < studentNum; j++)
+            {
+                for(int a = 0; a < pairNum; a++)
+                {
+                    if(pairs1[a] == i && pairs2[a] == j){
+                        isFriend[i][j] = true;
+                    }
+                }
+            }
         }
         if(studentNum < 2  || pairNum < (studentNum / 2)){
             results[repeat] = 0; continue;
         } else {
-            results[repeat] = howManyPairs(pairs1, pairs2, pairNum, studentNum);
+            results[repeat] = howMany(pairs1, pairs2, isPaired, isFriend, pairNum, studentNum);
         }
     }
     for(int i = 0; i < totalCase; i++){
@@ -31,81 +66,57 @@ int main()
     return 0;
 }
 
-int maxPair(int studentNum)
+int compare(int x1, int x2, int y1, int y2)
 {
-    int result = 1;
-    for(int i = 0; i < studentNum; i += 2)
+    if(x1 > y1)
     {
-        result *= (i + 1);
+        return 1;
     }
-    return result;
+    else if(x1 < y1)
+    {
+        return -1;
+    }
+    else if(x2 > y2)
+    {
+        return 1;
+    }
+    else if(x2 < y2){
+        return -1;
+    } else {
+        return 0;
+    }
 }
-int howManyPairs(const int* pairs1, const int* pairs2, int pairNum, int studentNum)
+
+int total = 0;
+int howMany(const int* pairs1, const int* pairs2, bool isPaired[], bool isFriend[][10], int pairNum, int studentNum)
 {
-    bool isEveryonePaired = false;
+    bool success;
+    success = false;
+    for(int n = 0; n < studentNum; n++)
+    {
+        if(isPaired[n] == false)
+        {
+            success = false; break;
+        }
+        else{
+            success = true;
+        }
+    }
+    if(success)
+    {
+        return 1;
+    }
     for(int i = 0; i < studentNum; i++)
     {
-        isEveryonePaired = false;
-        for(int j = 0; j < pairNum; j++)
+        for(int j = i+1; j < studentNum; j++)
         {
-            if(pairs1[j] == i || pairs2[j] == i)
+            if(!isPaired[i] && !isPaired[j] && isFriend[i][j])
             {
-                isEveryonePaired = true;
+                isPaired[i] = true; isPaired[j] = true;
+                total += howMany(pairs1, pairs2, isPaired, isFriend, pairNum, studentNum);
+                isPaired[i] = false; isPaired[j] = false; 
             }
-        }
-        if(isEveryonePaired == false){
-            return 0;
         }
     }
-    bool changed = true;
-    bool* isPaired = new bool[studentNum]; 
-    int result = 0;
-    int *firstY = new int[pairNum];
-    int start = 0;
-    for(int n = 0; n < pairNum; n++){
-        firstY[n] = 0;
-    }
-    for(int m = 0; m < maxPair(studentNum); m++)
-    {
-        for(int i = 0; i < studentNum; i++){
-            isPaired[i] = false;
-        }
-        for(int x = 0; x < studentNum; x++)
-        {
-            if(isPaired[x] == true){
-                continue;
-            } else if(changed == true){
-                start = firstY[x];
-            } else {
-                start = firstY[x] + 1;
-            }
-            for(int y = start; y < pairNum; y++)
-            {
-                if(x == pairs1[y] && isPaired[pairs2[y]] == false)
-                {
-                    isPaired[x] = true; isPaired[pairs2[y]] = true;
-                }
-                else if(x == pairs2[y] && isPaired[pairs1[y]] == false)
-                {
-                    isPaired[x] == true; isPaired[pairs1[y]] == true;
-                }
-                if(y != firstY[x]){
-                    changed = true;
-                } else {
-                    changed = false;
-                }
-                firstY[x] = y;
-            }
-        }
-        bool success = true;
-        for(int i = 0; i < studentNum; i++){
-            if(isPaired[i] == false){
-                success = false; break;
-            }
-        }
-        if(success == true){
-            result++;
-        }
-    }
-    return result;
+    return total;
 }
