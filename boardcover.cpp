611b580816f95ea7cbeaf820d53087c971a6b959
hsh814 <hsh814@naver.com>
2019-09-04 22:20:05 +0900

    Basic board and cover type

--- boardcover.cpp
+++ boardcover.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 
 bool board[20][20];
+const int cover[4][2][2] = {
+    {{1, 0}, {0, -1}},
+    {{1, 0}, {1, -1}},
+    {{0, -1}, {1, -1}},
+    {{-1, -1}, {0, -1}}
+};
 int height, width;
 
 void clearBoard()
@@ -18,6 +24,21 @@ void clearBoard()
     }
 }
 
+int countWhite()
+{
+    int whiteNum = 0;
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(!board[i][j]){
+                whiteNum++;
+            }
+        }
+    }
+    return whiteNum;
+}
+
 int main()
 {
     //input
@@ -27,8 +48,11 @@ int main()
     string temp = "";
     for(int repeat = 0; repeat < totalCase; repeat++)
     {
-        clearBoard();
         cin >> height >> width;
+        if(height < 1 || height > 20 || width < 1 || width > 20){
+            results[repeat] = 0; continue;
+        }
+        clearBoard();
         for(int i = 0; i < height; i++)
         {
             cin >> temp;
@@ -40,8 +64,24 @@ int main()
                 }
             }
         }
+        int whiteNum = countWhite();
+        if(whiteNum > 50 || whiteNum % 3 != 0)
+        {
+            results[repeat] = 0; continue;
+        }
         //output
-        
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                if(board[i][j]){
+                    cout << "O";
+                } else {
+                    cout << "X";
+                }
+            }
+            cout << endl;
+        }
     }
     for(int i = 0; i < totalCase; i++){
         cout << results[i] << endl;
